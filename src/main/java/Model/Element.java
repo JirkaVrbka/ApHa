3f43556f81d82d1cf165016fdaeeea04ffe132bd
Jirka <jiri.vrbka@gmail.com>
2019-02-05 23:43:29 +0100

    Position and Abilities into class, SelectorType and DriverType into enum

--- src/main/java/Model/Element.java
+++ src/main/java/Model/Element.java
@@ -2,32 +2,147 @@ package Model;
 
 import Constants.PositionType;
 
-import java.util.List;
-
 public class Element {
+
+    public enum SelectorType{
+        ID,
+        XPATH,
+        PARTIAL_ID,
+        TEXT
+    }
+
+    public enum DriverType{
+        APPIUM,
+        SELENIUM
+    }
+
+    public class ElementAbilities {
+
+        // abilities
+        private boolean clickable;
+        private boolean hasText;
+        private boolean redirects;
+        private String redirectsToPom;
+
+        public boolean isClickable() {
+            return clickable;
+        }
+
+        public void setClickable(boolean clickable) {
+            this.clickable = clickable;
+        }
+
+        public boolean isHasText() {
+            return hasText;
+        }
+
+        public void setHasText(boolean hasText) {
+            this.hasText = hasText;
+        }
+
+        public boolean isRedirects() {
+            return redirects;
+        }
+
+        public void setRedirects(boolean redirects) {
+            this.redirects = redirects;
+        }
+
+        public String getRedirectsToPom() {
+            return redirectsToPom;
+        }
+
+        public void setRedirectsToPom(String redirectsToPom) {
+            this.redirectsToPom = redirectsToPom;
+        }
+    }
+
+    public class Position {
+
+        private PositionType topLeftCornerPT;
+        private int topLeftCornerValue;
+        private PositionType heightPT;
+        private int heightValue;
+        private PositionType lenghtPT;
+        private int lengthValue;
+
+        public PositionType getTopLeftCornerPT() {
+            return topLeftCornerPT;
+        }
+
+        public void setTopLeftCornerPT(PositionType topLeftCornerPT) {
+            this.topLeftCornerPT = topLeftCornerPT;
+        }
+
+        public int getTopLeftCornerValue() {
+            return topLeftCornerValue;
+        }
+
+        public void setTopLeftCornerValue(int topLeftCornerValue) {
+            this.topLeftCornerValue = topLeftCornerValue;
+        }
+
+        public PositionType getHeightPT() {
+            return heightPT;
+        }
+
+        public void setHeightPT(PositionType heightPT) {
+            this.heightPT = heightPT;
+        }
+
+        public int getHeightValue() {
+            return heightValue;
+        }
+
+        public void setHeightValue(int heightValue) {
+            this.heightValue = heightValue;
+        }
+
+        public PositionType getLenghtPT() {
+            return lenghtPT;
+        }
+
+        public void setLenghtPT(PositionType lenghtPT) {
+            this.lenghtPT = lenghtPT;
+        }
+
+        public int getLengthValue() {
+            return lengthValue;
+        }
+
+        public void setLengthValue(int lengthValue) {
+            this.lengthValue = lengthValue;
+        }
+    }
 
     // required fields
     private String name;
-    private String selector;
-    private String selectorString;
-    private String requiredDriver;
-    private List<String> pom;
+    private SelectorType selectorType;
+    private String selectorValue;
+    private DriverType requiredDriver;
+
+    private ElementAbilities abilities;
 
     // optional fields
     private String imagePath;
-    private PositionType topLeftCornerPT;
-    private int topLeftCornerValue;
-    private PositionType heightPT;
-    private int heightValue;
-    private PositionType lenghtPT;
-    private int lengthValue;
 
-    // abilities
-    private boolean clickable;
-    private boolean hasText;
-    private boolean redirects;
-    private String redirectsToPom;
+    private Position position;
 
+    public ElementAbilities getAbilities() {
+        return abilities;
+    }
+
+    public void setAbilities(ElementAbilities abilities) {
+        this.abilities = abilities;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 
     public String getName() {
         return name;
@@ -37,36 +152,28 @@ public class Element {
         this.name = name;
     }
 
-    public String getSelector() {
-        return selector;
+    public SelectorType getSelectorType() {
+        return selectorType;
     }
 
-    public void setSelector(String selector) {
-        this.selector = selector;
+    public void setSelectorType(SelectorType selectorType) {
+        this.selectorType = selectorType;
     }
 
-    public String getSelectorString() {
-        return selectorString;
+    public String getSelectorValue() {
+        return selectorValue;
     }
 
-    public void setSelectorString(String selectorString) {
-        this.selectorString = selectorString;
+    public void setSelectorValue(String selectorValue) {
+        this.selectorValue = selectorValue;
     }
 
-    public String getRequiredDriver() {
+    public DriverType getRequiredDriver() {
         return requiredDriver;
     }
 
-    public void setRequiredDriver(String requiredDriver) {
+    public void setRequiredDriver(DriverType requiredDriver) {
         this.requiredDriver = requiredDriver;
-    }
-
-    public List<String> getPom() {
-        return pom;
-    }
-
-    public void setPom(List<String> pom) {
-        this.pom = pom;
     }
 
     public String getImagePath() {
@@ -75,85 +182,5 @@ public class Element {
 
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
-    }
-
-    public PositionType getTopLeftCornerPT() {
-        return topLeftCornerPT;
-    }
-
-    public void setTopLeftCornerPT(PositionType topLeftCornerPT) {
-        this.topLeftCornerPT = topLeftCornerPT;
-    }
-
-    public int getTopLeftCornerValue() {
-        return topLeftCornerValue;
-    }
-
-    public void setTopLeftCornerValue(int topLeftCornerValue) {
-        this.topLeftCornerValue = topLeftCornerValue;
-    }
-
-    public PositionType getHeightPT() {
-        return heightPT;
-    }
-
-    public void setHeightPT(PositionType heightPT) {
-        this.heightPT = heightPT;
-    }
-
-    public int getHeightValue() {
-        return heightValue;
-    }
-
-    public void setHeightValue(int heightValue) {
-        this.heightValue = heightValue;
-    }
-
-    public PositionType getLenghtPT() {
-        return lenghtPT;
-    }
-
-    public void setLenghtPT(PositionType lenghtPT) {
-        this.lenghtPT = lenghtPT;
-    }
-
-    public int getLengthValue() {
-        return lengthValue;
-    }
-
-    public void setLengthValue(int lengthValue) {
-        this.lengthValue = lengthValue;
-    }
-
-    public boolean isClickable() {
-        return clickable;
-    }
-
-    public void setClickable(boolean clickable) {
-        this.clickable = clickable;
-    }
-
-    public boolean isHasText() {
-        return hasText;
-    }
-
-    public void setHasText(boolean hasText) {
-        this.hasText = hasText;
-    }
-
-    public boolean isRedirects() {
-        return redirects;
-    }
-
-    public void setRedirects(boolean redirects) {
-        this.redirects = redirects;
-    }
-
-    public String getRedirectsToPom() {
-        return redirectsToPom;
-    }
-
-    public void setRedirectsToPom(String redirectsToPom) {
-        this.redirectsToPom = redirectsToPom;
     }
 }
